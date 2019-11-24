163848e021189d9eedae69ccea271071e93d1e93
Kate <kate.kalinchenko@gmail.com>
2019-11-24 23:33:29 +0200

    gallery (minor changes)

--- JavaScript/WebContent/js4/task.js
+++ JavaScript/WebContent/js4/task.js
@@ -24,63 +24,58 @@ images[19]="img/img18.jpg";
 
 class Gallery {
 	
-    constructor(images) {
+	constructor(images) {
         this.images = images;
     }
- changeImg(){
-	document.slide.src = images[i];
-	if(i<images.length-1){
-		i++;
-	}else{
-		i=0;
+	changeImg(){
+		document.slide.src = images[i];
+		if(i<images.length-1){
+			i++;
+		}else{
+			i=0;
+		}
 	}
-}
 
- prevImg(){
-	if(i>0){
-		i--;
-	}else{
-		i=images.length-1;
-	}
+	prevImg(){
+		if(i>0){
+			i--;
+		}else{
+			i=images.length-1;
+		}
 	document.slide.src = images[i];
-}
-
- nextImg(){
-	if(i<images.length-1){
-		i++;
-	}else{
-		i=0;
 	}
-	document.slide.src = images[i];
 
-}
+	nextImg(){
+		if(i<images.length-1){
+			i++;
+		}else{
+			i=0;
+		}
+		document.slide.src = images[i];
+	}
  
-  run() {
-
- let time;
- let timer;
- let reg = /^[0-9]+$/;
- let user = prompt("Time (ms): ");
- if (reg.test(user)) {
-     time = +user;
-     timer = setInterval(function () {
-    	 if(i<images.length-1){
-    			i++;
-    		}else{
-    			i=0;
-    		}
-    		document.slide.src = images[i];
-     }, time);
- } else {
-     alert("enter number")
- }
- stop.onclick = function(){
-	  clearInterval(timer);
- }
-}
-  
-  
-}
+	run() {
+		let time;
+		let timer;
+		let reg = /^[0-9]+$/;
+		let user;
+		while(!reg.test(user)){
+			user=prompt("Time (ms): ");
+		}
+		time = +user;
+		timer = setInterval(function () {
+			if(i<images.length-1){
+				i++;
+			}else{
+				i=0;
+			}
+    	document.slide.src = images[i];
+		}, time);
+		stop.onclick = function(){
+			clearInterval(timer);
+		}
+	}
+  }
 
 
 let gallery = new Gallery(images);
@@ -92,4 +87,3 @@ let stop = document.getElementById("stop_slideshow");
 next.onclick = gallery.nextImg;
 previous.onclick = gallery.prevImg;
 run.onclick = gallery.run;
-stop.onclick = gallery.stop;
