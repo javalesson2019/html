fb6d73842abbfeac9bee5b48401a5e35a3dbace4
Maxim <maxshaptala@gmail.com>
2019-12-18 14:03:41 +0200

    Проверена домашка 46. Смотри отзыв в review.md

--- JavaScript/WebContent/js4/task.js
+++ JavaScript/WebContent/js4/task.js
@@ -1,81 +1,64 @@
-var i=1;
-var images=[];
-
-images[0]="img/img0.jpg";
-images[1]="img/img1.jpg";
-images[2]="img/img2.jpg";
-images[3]="img/img3.jpg";
-images[4]="img/img4.jpg";
-images[5]="img/img5.jpg";
-images[6]="img/img6.jpg";
-images[7]="img/img7.jpg";
-images[8]="img/img7.jpg";
-images[9]="img/img8.jpg";
-images[10]="img/img9.jpg";
-images[11]="img/img10.jpg";
-images[12]="img/img11.jpg";
-images[13]="img/img12.jpg";
-images[14]="img/img13.jpg";
-images[15]="img/img14.jpg";
-images[16]="img/img15.jpg";
-images[17]="img/img16.jpg";
-images[18]="img/img17.jpg";
-images[19]="img/img18.jpg";
+var i = 1;
+var images = [];
+// Циклом нельзя было как-то обойтись?
+for (let i = 0; i < 19; i++) {
+	images.push(`img/img${i}.jpg`);
+}
 
 class Gallery {
-	
+
 	constructor(images) {
-        this.images = images;
-    }
-	changeImg(){
+		this.images = images;
+	}
+	changeImg() {
 		document.slide.src = images[i];
-		if(i<images.length-1){
+		if (i < images.length - 1) {
 			i++;
-		}else{
-			i=0;
+		} else {
+			i = 0;
 		}
 	}
 
-	prevImg(){
-		if(i>0){
+	prevImg() {
+		if (i > 0) {
 			i--;
-		}else{
-			i=images.length-1;
-		}
-	document.slide.src = images[i];
-	}
-
-	nextImg(){
-		if(i<images.length-1){
-			i++;
-		}else{
-			i=0;
+		} else {
+			i = images.length - 1;
 		}
 		document.slide.src = images[i];
 	}
- 
+
+	nextImg() {
+		if (i < images.length - 1) {
+			i++;
+		} else {
+			i = 0;
+		}
+		document.slide.src = images[i];
+	}
+
 	run() {
 		let time;
 		let timer;
 		let reg = /^[0-9]+$/;
 		let user;
-		while(!reg.test(user)){
-			user=prompt("Time (ms): ");
+		while (!reg.test(user)) {
+			user = prompt("Time (ms): ");
 		}
 		time = +user;
 		timer = setInterval(function () {
-			if(i<images.length-1){
+			if (i < images.length - 1) {
 				i++;
-			}else{
-				i=0;
+			} else {
+				i = 0;
 			}
-    	document.slide.src = images[i];
+			document.slide.src = images[i];
 		}, time);
-		stop.onclick = function(){
+		stop.onclick = function () {
 			clearInterval(timer);
 		}
 	}
-  }
+}
 
 
 let gallery = new Gallery(images);
